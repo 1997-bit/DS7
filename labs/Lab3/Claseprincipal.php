a50ed41f41c4a3827c83c8180aa64ab6c26e2ec2
ionathh <jonathgommz307@gmail.com>
2026-04-22 16:25:08 -0500

    ACcidente

---
+++ labs/Lab3/Claseprincipal.php
@@ -0,0 +1,46 @@
+<?php
+ class Claseprincipal
+ {
+
+ public $Nombre;
+ public $CorreoElectronico;
+ public $Cedula;
+ public $Edad;
+
+ public function __construct($nombre, $CorreoElectronico, $cedula, $edad)
+ {
+    $this->Nombre = $nombre;
+    $this->CorreoElectronico = $CorreoElectronico;
+    $this->Cedula = $cedula;
+    $this->Edad = $edad;
+ }
+
+ public function mostrarContacto()
+ {
+    echo "Nombre: ". $this->Nombre . "<br>";
+    echo "CorreoElectronico". $this->CorreoElectronico . "<br>";
+    echo "Cedula". $this->Cedula . "<br>";
+    echo "Edad". $this->Edad . "<br>";
+ }
+
+ public function mostrarServidor()
+ {
+
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+        echo "PHP_SELF: "       . $_SERVER['PHP_SELF']        . "<br>";
+        echo "SERVER_NAME: "    . $_SERVER['SERVER_NAME']      . "<br>";
+        echo "USER_AGENT: "     . $_SERVER['HTTP_USER_AGENT']  . "<br>";
+        echo "REQUEST_METHOD: " . $_SERVER['REQUEST_METHOD']   . "<br>";
+        echo "REMOTE_ADDR: "    . $_SERVER['REMOTE_ADDR']      . "<br>";
+        echo "QUERY_STRING: "   . $_SERVER['QUERY_STRING']     . "<br>";
+ }
+
+ }
+
+
+?>
--- labs/Lab3/formularioP.php
+++ labs/Lab3/formularioP.php
@@ -7,40 +7,83 @@
 */
 
 
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>FormularioPOST</title>
+
+        <style>
+            body
+            {
+                background-color: rgba(93, 81, 226, 0.73);
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                height: 100vh;
+                font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+           }
+
+            section
+            {
+                background: rgba(15, 234, 15, 0.73);
+                padding: 30px;
+                border-radius: 15px;
+                box-shadow: 0 10px 25px rgba(37, 7, 136, 0.73);
+                width: 190px;
+            }
+
+            input
+            {
+                padding: 10px;
+                border: 3px solid #754404;
+                border-radius: 8px;
+                transition: 0.3s;
+            }
+
+           #Enviar
+           {
+            cursor: pointer;
+            margin-left: 50px;
+           }
+
+           #Enviar:hover
+           {
+                color: red;
+                background: #e6df08;
+
+           }
+
+        </style>
+
+</head>
 <body>
     
-<section 
-<h1>hola mundo</h1>
-    <form id="fomularioP" action="procesar.php" method="POST">
-    <input type="text" name="nombre" placeholder="Nombre" "required">
+<section>
+    <form id="fomularioP" action="salidaP.php" method="POST">
+    <input type="text" name="nombre" placeholder="ingrese su nombre" required>
     <br>
     <br>
     
-    <input type="text" name="Correo Electronico" placeholder="CorreoElectronico""><br>
-    <br>
-
-    <input type="text" name="cedula" placeholder="Cedula"> 
+    <input type="email" name="CorreoElectronico" placeholder="ejemplo@gmail.com" required >
     <br>
     <br>
 
-    <input type="text" name="edad" placeholder="Edad"> 
-    <input type="submit" value="Enviar">
+    <input type="text" name="Cedula" placeholder="0000-0000-0000" required> 
+    <br>
+    <br>
+
+    <input type="text" name="Edad" placeholder="Edad" required > 
+    <br>
+    <br> 
+    <input type="submit" value="Enviar" id="Enviar">
+    <br>
     </form>
 
 </section>
 
 </body>
 
-<style>
-
-#formularioP{
-    display: table-column;
-    : green;
-    
-
-}
-
-    
-</style>
+</html>
