db2ab687642d9ea63fc253dd7be2f5bc4afaeeeb
1997-bit <132793825+1997-bit@users.noreply.github.com>
2026-04-22 18:21:08 -0500

    chore(sync): traer formularioP y salidaP desde jonathan@1d47ee6
    Co-authored-by: ionathh <jonathgommz307@gmail.com>

--- labs/Lab3/formularioG.php
+++ labs/Lab3/formularioG.php
@@ -1,7 +1,60 @@
 <?php
 // IMC GET
 ?>
+        <style>
+            body
+            {
+                background-color: rgba(93, 81, 226, 0.73);
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                height: 100vh;
+                font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+           }
 
+            section
+            {
+                background: rgba(15, 234, 15, 0.73);
+                padding: 30px;
+                border-radius: 15px;
+                box-shadow: 0 10px 25px rgba(37, 7, 136, 0.73);
+                width: 190px;
+            }
+
+            input
+            {
+                padding: 10px;
+                border: 3px solid #754404;
+                border-radius: 8px;
+                transition: 0.3s;
+            }
+
+           #Enviar
+           {
+            cursor: pointer;
+            margin-left: 50px;
+           }
+
+           #Enviar:hover
+           {
+                color: red;
+                background: #e6df08;
+
+           }
+
+           #IrIMC {
+                 cursor: pointer;
+                 margin-left: 46px;
+                 margin-top: 10px;
+            }
+
+            #IrIMC:hover {
+                color: red;
+                background: #e6df08;
+            }
+
+        </style>
 <body>
     <form action="salidaG.php" method="GET">
     Nombre:<br>
@@ -9,7 +62,7 @@
     Peso (kg):<br>
     <input type="number" name="peso" required><br><br>
     Altura (m):<br>
-    <input type="number" name="altura" required><br><br>
+    <input type="number" name="altura" step="any" required><br><br>
     <input type="submit" value="Calcular IMC">
     <input type="button" value="Regresar" id="IrConacto" onclick="window.location.href='formularioP.php'">
 
@@ -18,3 +71,4 @@
     </form>
 
 </body>
+
--- labs/Lab3/salidaG.php
+++ labs/Lab3/salidaG.php
@@ -1,22 +1,21 @@
 <?php
 require_once "Claseprincipal.php";
 
-$obj = new ClasePrincipal();
-$datos = $obj->calcularIMCGET();
-$server = $obj->serverData();
+$obj = new Claseprincipal();
+
+$nombre = $_GET["nombre"];
+$peso   = $_GET["peso"];
+$altura = $_GET["altura"];
 ?>
 
 <h2>Resultado IMC (GET)</h2>
 
-Nombre: <?= $datos["nombre"] ?><br>
-Peso: <?= $datos["peso"] ?> kg<br>
-Altura: <?= $datos["altura"] ?> m<br>
-IMC: <?= number_format($datos["imc"],2) ?><br>
+<?php
+$obj->calcularIMC($nombre,$peso,$altura);
+?>
 
 <h2>Datos del servidor</h2>
 
 <?php
-foreach($server as $k=>$v){
-    echo $k . " : " . $v . "<br>";
-}
+$obj->mostrarServidor();
 ?>
