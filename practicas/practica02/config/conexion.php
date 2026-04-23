fef48eb2b942cc4179cd2d99f412dd763f3e0c34
gloria-utp <gloria.moreno@utp.ac.pa>
2026-04-22 21:28:44 -0500

    Conexion --v2

--- practicas/practica02/config/conexion.php
+++ practicas/practica02/config/conexion.php
@@ -1,8 +1,28 @@
-class Conexion {
-    public static function Conectar() {
+<?php
+class Conexion
+{
+    public static function Conectar()
+    {
 
-    //Datos de conexion
-    $host = 'localhost';
-    $baseDeDatos = 
+        //Datos de conexion
+        $host = 'localhost';
+        $baseDeDatos = 'bdjonas';
+        $usuario = 'root';
+        $contrasena = '';
+
+        //Crear conexion PDO
+        $conexion = new PDO(
+            "mysql: host=$host;dbname=$baseDeDatos;charset=utf8",
+            $usuario,
+            $contrasena
+        );
+
+        //Lanzar excepciones en errores
+        $conexion->setAttribute(
+            PDO::ATTR_ERRMODE,
+            PDO::ERRMODE_EXCEPTION
+        );
+
+        return $conexion;
     }
 }
