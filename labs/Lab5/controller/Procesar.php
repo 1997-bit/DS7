a1719effa9773069c1e11a3d7989a3035e35ca10
1997-bit <132793825+1997-bit@users.noreply.github.com>
2026-05-07 21:00:13 -0500

    feat(lab5): login y registro completo funcional

--- labs/Lab5/controller/Procesar.php
+++ labs/Lab5/controller/Procesar.php
@@ -9,27 +9,26 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
 $accion = $_POST["accion"] ?? "";
 
   // ROUTER
+
 if ($accion === "registro") {
 
     $usuario = trim($_POST["usuario"] ?? "");
     $contrasena = $_POST["contrasena"] ?? "";
 
-    if ($usuario === "" || $contrasena === "") {
-        exit("Campos vacíos");
+    if ($usuario === "" || $contrasena === "") exit("Campos vacíos");
+    if (strlen($usuario) < 3 || strlen($usuario) > 11) exit("Usuario 3–11");
+    if (strlen($contrasena) < 5) exit("Pass min 5");
+
+    try {
+        $model = new Usuario();
+        $model->registrar($usuario, $contrasena);
+
+        header("Location: ../view/Login.php?registro=ok");
+        exit;
+
+    } catch (PDOException $e) {
+       exit("ERROR");
     }
-
-    if (strlen($usuario) < 3 || strlen($usuario) > 10) {
-        exit("Usuario inválido");
-    }
-
-    if (strlen($contrasena) < 5) {
-        exit("Password muy corta");
-    }
-
-  
-
-    header("Location: ../view/Login.php");
-    exit;
 }
 
 // LONGIN
@@ -38,12 +37,24 @@ if ($accion === "login") {
     $usuario = trim($_POST["usuario"] ?? "");
     $contrasena = $_POST["contrasena"] ?? "";
 
-    if ($usuario === "" || $contrasena === "") {
-        exit("Campos vacíos");
-    }
+    if ($usuario === "" || $contrasena === "") exit("Campos vacíos");
 
-    header("Location: ../view/Formulario.php");
-    exit;
+    try {
+        $model = new Usuario();
+        $data = $model->login($usuario, $contrasena);
+
+        if (!$data) exit("Credenciales incorrectas");
+
+        session_start();
+        $_SESSION["id"] = $data["id"];
+        $_SESSION["usuario"] = $data["usuario"];
+
+        header("Location: ../view/Formulario.php");
+        exit;
+
+    } catch (PDOException $e) {
+        exit("ERROR");
+    }
 }
 
 
--- labs/Lab5/view/Registro.php
+++ labs/Lab5/view/Registro.php
@@ -1,22 +1,16 @@
 <?php
 
-
 ?>
-
 <!doctype html>
-<html lang="en">
+<html lang="es">
 	<head>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
 		<title>Registro</title>
-
 		<link rel="stylesheet" href="../assets/css/base.css" />
 		<link rel="stylesheet" href="../assets/css/registro.css" />
-
 		<link rel="icon" type="image/svg+xml" href="../assets/favicon.svg" />
 	</head>
-
 	<body>
 		<section>
 			<div id="Registro">
@@ -24,21 +18,29 @@
 					<input type="hidden" name="accion" value="registro" />
 					<h3>REGISTRO</h3>
 
-					<label>
-						Usuario:
-						<input type="text" name="usuario" required />
-					</label>
+					<label
+						>Usuario:
+						<input
+							type="text"
+							name="usuario"
+							minlength="3"
+							maxlength="11"
+							required
+						/> </label
+					><br /><br />
 
-					<br /><br />
-
-					<label>
-						Contraseña:
-						<input type="password" name="contrasena" required />
-					</label>
-
-					<br /><br />
+					<label
+						>Contraseña:
+						<input
+							type="password"
+							name="contrasena"
+							minlength="5"
+							required
+						/> </label
+					><br /><br />
 
 					<button type="submit">Registrar</button>
+					<p><a href="Login.php">Ya tengo cuenta</a></p>
 				</form>
 			</div>
 		</section>
