68777aa8f48bc8c1de101cd5f7fe54aa9c51cda0
gloria-utp <gloria.moreno@utp.ac.pa>
2026-05-19 15:46:19 -0500

    Agregar manejo de sesiones y logout

--- parciales/parcial-02/core/App.php
+++ parciales/parcial-02/core/App.php
@@ -1,13 +1,35 @@
 <?php
 
-class App {
-    public static function run(): void {
-        session_start();
-        define('BASE_PATH', realpath(__DIR__.'/..').'/');
+class App
+{
 
-        require BASE_PATH.'config/Conexion.php';
-        require BASE_PATH.'core/Router.php';
-        require BASE_PATH.'core/Security.php';
+    public static function run(): void
+    {
+
+        session_start();
+
+        $timeout = 1800;
+
+        if (isset($_SESSION['LAST_ACTIVITY'])) {
+
+            if (time() - $_SESSION['LAST_ACTIVITY'] > $timeout) {
+
+                session_unset();
+                session_destroy();
+
+                header("Location: /login?error=sesion");
+                exit;
+            }
+        }
+
+        $_SESSION['LAST_ACTIVITY'] = time();
+
+        define('BASE_PATH', realpath(__DIR__ . '/..') . '/');
+
+        require BASE_PATH . 'config/Conexion.php';
+        require BASE_PATH . 'core/Router.php';
+        require BASE_PATH . 'core/Security.php';
+
         (new Router())->dispatch();
     }
 }
